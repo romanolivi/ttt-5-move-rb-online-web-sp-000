964dbcd3f8ab239b40d500389fcae8c8a158b9ce
Romano Livi <romano_livi@outlook.com>
2020-12-08 23:09:56 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,6 +6,9 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def initialize(board = nil)
+  @board = board || Array.new(9," ")
+
 def input_to_index(num)
   num.to_i - 1 
 end
